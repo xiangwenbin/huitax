41cbed0a349c08d77320e706c4eb2e3c4d30505e
roly2000 <roly2000@163.com>
2017-01-03 15:22:22 +0800

    db
    Signed-off-by: roly2000 <roly2000@163.com>

---
+++ src/main/java/com/zhonghui/huitax/model/ZhUser.java
@@ -0,0 +1,92 @@
+package com.zhonghui.huitax.model;
+
+import java.io.Serializable;
+
+public class ZhUser implements Serializable{
+    private int id;
+    private String username;
+    private String nickname;
+    private String loginname;
+    private String mobile;
+    private String loginpassword;
+    private String url;
+    private String address;
+    private int integral;
+    private int status;
+    private long createtime;
+    private long updatetime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getLoginname() {
+		return loginname;
+	}
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getLoginpassword() {
+		return loginpassword;
+	}
+	public void setLoginpassword(String loginpassword) {
+		this.loginpassword = loginpassword;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getIntegral() {
+		return integral;
+	}
+	public void setIntegral(int integral) {
+		this.integral = integral;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public long getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(long createtime) {
+		this.createtime = createtime;
+	}
+	public long getUpdatetime() {
+		return updatetime;
+	}
+	public void setUpdatetime(long updatetime) {
+		this.updatetime = updatetime;
+	}
+    
+    
+}
